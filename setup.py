f30dcc8dc106cf15ba747f87cd0372f0084fdab0
kotisoujanya <kotisoujanya@gmail.com>
2023-03-28 18:20:12 +0530

    demo pipeline folders

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(name="censes-income",
+      version="0.0.1",
+      author="soujanya",
+      author_email="kotisoujanya@gmail.com",
+      packages=find_packages(),
+      install_requires=['pandas','numpy','flask']
+      )
